aeef48689656a3b81a128389188f5b1d02c96106
famousDeer <dawidtrzcinski1@gmail.com>
2024-01-19 13:46:36 +0100

    Created form for travel-manager page

--- my_website/travel_manager/forms.py
+++ my_website/travel_manager/forms.py
@@ -4,3 +4,5 @@ from django_countries.fields import CountryField
 class CreateNewDestination(forms.Form):
     country = CountryField().formfield(label="country")
     city = forms.CharField(label="city", max_length=100)
+    tiktok = forms.CharField(label="tiktok", max_length=200)
+    instagram = forms.CharField(label="instagram", max_length=200)
