c196f8dd911bb3496477f76bbcd65852a82be3ec
famousDeer <dawidtrzcinski1@gmail.com>
2024-01-19 22:14:59 +0100

    Added delete handle and add description

--- my_website/travel_manager/urls.py
+++ my_website/travel_manager/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('travel-manager', views.travel_manager, name='travel-manager'),
     path('add-destination', views.add_destination, name='add-destination'),
     path('travel-manager/<int:id>', views.index, name='info-destination'),
+    path("travel-manager/delete/<int:id>", views.delete, name="delete"),
+    path('travel-manager/add-description/<int:id>', views.add_description, name='add_description'),
 ]
