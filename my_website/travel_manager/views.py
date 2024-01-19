f7a5e750e5d6f42a080adef17a34a5642b1f8008
famousDeer <dawidtrzcinski1@gmail.com>
2024-01-19 22:15:40 +0100

    Handle view of delete destination and view of adding description

--- my_website/travel_manager/views.py
+++ my_website/travel_manager/views.py
@@ -1,9 +1,24 @@
-from django.shortcuts import render
-from django.http import HttpResponseRedirect
-from .models import Destinations, Tiktok, Instagram
+from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect, HttpResponse
+from django.contrib import messages
+from django.urls import reverse
+from .models import Destinations, Tiktok, Instagram, Information
 from .forms import CreateNewDestination
 
 # Create your views here.
+
+def add_description(request, id):
+    if request.method == "POST":
+        destination = Destinations.objects.get(id=id)
+        information = Information.objects.filter(destinations=destination).first()
+        description = request.POST.get(f"descriptions{id}")
+        if information is not None:
+            information.description = description
+            information.save()
+        else:
+            information = Information.objects.create(destinations=destination, fly_cost=0.0, description=description)
+    return render(request, 'main/info_destination.html', {"destinations":destination, "information": information})
+
 def add_destination(request):
     if request.method == "POST":
         form = CreateNewDestination(request.POST)
@@ -30,12 +45,18 @@ def add_destination(request):
 
     return render(request, "main/add_destination.html", {"form":form})
 
+def delete(request, id):
+    destination = Destinations.objects.get(id=id)
+    messages.info(request, f"Successfully deleted {destination.country.name}")
+    destination.delete()
+    return redirect(reverse('travel-manager'))
+
 def travel_manager(request):
     #TODO: Render information from database
     return render(request, "main/travel_manager.html")
 
 def index(request, id):
-    destinations = Destinations.objects.get(id=id)
-    return render(request, "main/info_destination.html", {"destinations": destinations})
-
+    destination = Destinations.objects.get(id=id) 
+    information = Information.objects.filter(destinations=destination).first()
+    return render(request, "main/info_destination.html", {"destinations":destination, "information": information})
 
