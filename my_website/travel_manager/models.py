5e183d5f230d138b0c6fc89dcc68f8351025b326
famousDeer <dawidtrzcinski1@gmail.com>
2024-01-19 22:13:05 +0100

    Remove str representation

--- my_website/travel_manager/models.py
+++ my_website/travel_manager/models.py
@@ -11,16 +11,10 @@ class Destinations(models.Model):
     country = CountryField()
     city = models.CharField(max_length=100)
 
-    def __str__(self) -> str:
-        return self.city
-
 class Information(models.Model):
     destinations = models.ForeignKey(Destinations, on_delete=models.CASCADE)
     fly_cost = models.FloatField()
-    description = models.CharField(max_length=1000)
-
-    def __str__(self) -> str:
-        return self.description
+    description = models.TextField()
     
 class Tiktok(models.Model):
     destinations = models.ForeignKey(Destinations, on_delete=models.CASCADE)
