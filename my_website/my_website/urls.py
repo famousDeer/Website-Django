05cae7c90aed54347137f8972fd92114390f0e0b
famousDeer <dawidtrzcinski1@gmail.com>
2024-04-05 14:16:57 +0200

    Static fixed

--- my_website/my_website/urls.py
+++ my_website/my_website/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from register import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,4 +28,4 @@ urlpatterns = [
     path('', include("register.urls")),
     path('', include("travel_manager.urls")),
     path('', include("board_games.urls")),
-]
+]+ static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
