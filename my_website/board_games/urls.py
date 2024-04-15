a8f6f6162ac7ac56ee5845d9c2477a73d1f8daa6
famousDeer <dawidtrzcinski1@gmail.com>
2024-04-15 18:04:17 +0200

    Added board games tab with add games and list games

--- my_website/board_games/urls.py
+++ my_website/board_games/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from django.contrib.auth.decorators import login_required
 from . import views
 
 urlpatterns = [
-    path('board-games', views.BoardGame.as_view(), name='board-games'),
+    path('board-games/add-game', login_required(views.AddGame.as_view(), login_url='login'), name='add-game'),
+    path('board-games/list-game', login_required(views.ListView.as_view(), login_url='login'), name='list-game'),
 ]
